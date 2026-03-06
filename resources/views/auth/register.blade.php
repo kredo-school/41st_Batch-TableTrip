eef23b7ee3b440b8b31a25ca9b9109f4d3c52b22
riku0117 <riku2001117@yahoo.co.jp>
2026-03-06 21:28:49 +0900

    Merge branch 'main' into UI/test/Riku
    # Conflicts:
    #	routes/web.php

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,11 +1,16 @@
+@extends('layouts.app')
+
+@section('title', 'Login')
+
+@section('content')
 <x-guest-layout>
-    <form method="POST" action="{{ route('register') }}">
+    <form method="POST" action="{{ route('register.store') }}">
         @csrf
 
         <!-- Name -->
         <div>
             <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+            {{-- <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" /> --}}
             <x-input-error :messages="$errors->get('name')" class="mt-2" />
         </div>
 
@@ -50,3 +55,4 @@
         </div>
     </form>
 </x-guest-layout>
+@endsection
