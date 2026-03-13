2edad1a0c36bf89098750b409b750b68c3dd6584
Yui Takaishi <memorylaneclassic@yahoo.co.jp>
2026-03-13 06:03:37 -0700

    Built Admin Dashboard Page

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -4,11 +4,33 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Order;
+use Carbon\Carbon;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('admin.dashboard');
-    }
+        $ordersPerDay = [];
+        $labels = [];
+
+        for ($i = 6; $i >= 0; $i--) {
+
+            $date = Carbon::today()->subDays($i);
+
+            $labels[] = $date->format('D');
+
+            $ordersPerDay[] = Order::whereDate('created_at', $date)->count();
+        }
+
+        $recentOrders = Order::latest()
+            ->take(5)
+            ->get();
+
+        return view('admin.dashboard', compact(
+            'recentOrders',
+            'labels',
+            'ordersPerDay'
+        ));
+}
 }
