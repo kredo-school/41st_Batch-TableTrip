88aaafb2860397d8c2cd5982baac1d9b922b6fea
riku0117 <riku2001117@yahoo.co.jp>
2026-03-14 11:06:28 +0900

    cart

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -38,7 +38,7 @@
                                 {{-- guests --}}
                                 <td>{{ $reservation->number_of_guests }}</td>
                                 <td class="edit-icons">
-                                    <a href="{{ route('reservation.edit',$reservation->id) }}" class="">
+                                    {{-- <a href="{{ route('reservation.edit',$reservation->id) }}" class=""> --}}
                                         <i class="fa-regular fa-calender-check"></i>
                                     </a>
                                     <i class="fa-solid fa-user" style="margin-left:5px;"></i>
@@ -52,7 +52,9 @@
                         @endforelse
                     </tbody>
                 </table>
-                <a href="{{ route('reservations.index') }}" class="view-all">View All</a>
+                <div class="btn-container ">
+                    <a href="{{ route('reservations.index') }}" class="view-all btn-back">View Reservations</a>
+                </div>
             </div>
         </section>
         {{-- cart part --}}
@@ -60,7 +62,7 @@
             <h3><i class="fa-solid fa-cart-shopping"></i>Cart</h3>
             <div class="card-content">
                     <div class="item-grid">
-                        {{-- @forelse ($cart_items->take(2) as $item )
+                        @forelse ($cart_items->take(2) as $item )
                             <div class="cart-mini-item">
                                 <div class="mini-img">
                                     <img src="#" alt="" >
@@ -71,32 +73,43 @@
                                 </div>
                             </div>    
                         @empty
-                            <p class="empty-msg">No Carts yet</p>
-                        @endforelse --}}
-                    {{-- </div>
+                            <div class="empty-state">
+                                <p class="empty-msg">No Carts yet.</p>
+                            </div>
+                        @endforelse
+                    </div>
                     @if($cart_items->isNotEmpty())
                     <div class="cart-total-brief">
                         <p>Total: <span>{{number_format($totalPrice)}}</span></p>
                     </div>
-                    @endif --}}
-                    <a href="{{ route('cart') }}" class="view-all">View Cart</a>
+                    @endif
+                     <div class="btn-container">
+                         <a href="{{ route('cart') }}" class="btn-back">View Cart</a>
+                    </div>
+                    
             </div>
         </section>
 
         <section class="dashboard-card">
             <h3><i class="fa-solid fa-heart"></i> Favorite</h3>
             <div class="card-content">
-                <div class="filter-options">
+                <div class="filter-options" >
                     <label><input type="radio" name="fav" checked> Restaurants</label>
                     <label><input type="radio" name="fav"> Products</label>
                 </div>
+                <div class="btn-container">
+                         <a href=# class="btn-back">View Favorite</a>
                 </div>
+            </div>
         </section>
 
         <section class="dashboard-card">
             <h3><i class="fa-solid fa-bag-shopping"></i> Purchased</h3>
             <div class="card-content">
+                <div class="btn-container">
+                            <a href=# class="btn-back">View Order History</a>
                 </div>
+            </div>
         </section>
     </div>
 </div>
