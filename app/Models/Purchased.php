71f4cdb8fe61173e33caba84dc71b8d66aa77d8d
riku0117 <riku2001117@yahoo.co.jp>
2026-04-07 21:59:41 +0900

    amend a bit

--- app/Models/Purchased.php
+++ app/Models/Purchased.php
@@ -2,21 +2,15 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-
-<<<<<<< HEAD
-=======
-// クラス名をコントローラーの呼び出し（Purchased）に合わせる
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
->>>>>>> 775594f6f8b2f6e3709b9bd2ceddaaa2c756ab32
 class Purchased extends Model
 {
     use HasFactory;
 
     protected $table = 'purchased';
+
 
     protected $fillable = [
         'user_id',
@@ -27,28 +21,12 @@ class Purchased extends Model
         'status'
     ];
 
-    // public function meal_kit()
-    // {
-    //     return $this->belongsTo(MealKit::class, 'meal_kit_id');
-    // }
-}
-        'order_id',
-        'user_id',
-        'meal_kit_id',
-        'quantity',
-        'price_at_purchased',
-        'ordered_at',
+    protected $casts = [
+        'ordered_at' => 'datetime',
     ];
 
-    public function order()
-    {
-        return $this->belongsTo(Order::class, 'order_id');
-    }
-
-    public function product()
-    {
-        return $this->belongsTo(Product::class, 'meal_kit_id');
-    }
+    
+    
 
     public function user()
     {
