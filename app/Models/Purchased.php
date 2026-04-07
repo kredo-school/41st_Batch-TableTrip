aa1bdc71abd2535f612a28269c87487c2c5cd274
riku0117 <riku2001117@yahoo.co.jp>
2026-04-07 21:27:40 +0900

    amend a bit

--- app/Models/Purchased.php
+++ app/Models/Purchased.php
@@ -5,7 +5,6 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-// クラス名をコントローラーの呼び出し（Purchased）に合わせる
 class Purchased extends Model
 {
     use HasFactory;
