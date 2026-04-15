dba1d0454d0b6ca81fce32fa0187f8265f249c54
Sachiko <sckchqn426@gmail.com>
2026-04-15 22:26:27 +0900

    Product reviews

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -24,5 +24,10 @@ class Product extends Model
     {
         return $this->belongsTo(Restaurant::class);
     }
+
+    public function reviews()
+    {
+        return $this->hasMany(Review::class);
+    }
 }
  
