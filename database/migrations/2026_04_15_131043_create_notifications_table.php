49b8961074b1c6185f8831460ed86e02d56e7a52
riku0117 <riku2001117@yahoo.co.jp>
2026-04-15 22:32:23 +0900

    no message

---
+++ database/migrations/2026_04_15_131043_create_notifications_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+{
+    Schema::create('notifications', function (Blueprint $table) {
+        $table->id();
+        $table->morphs('recipient'); 
+    
+        $table->string('title');
+        $table->text('message');
+        
+        $table->nullableMorphs('target');
+        
+        $table->boolean('is_action_required')->default(false);
+        $table->boolean('is_completed')->default(false);
+        $table->timestamps();
+    });
+}
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('notifications');
+    }
+};
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
@@ -15,84 +14,42 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         // 1. Naoya Sato
-        // fixed user account
-        // first user
         User::create([
-            'first_name' => 'Naoya',
-            'last_name' => 'Sato',
-            'user_name' => 'iamnaoyasato',
-            'email' => 'satonao@kredo.com',
-            'password' => Hash::make('password914'),
-            'tel' => '627288238',
-            'country' => 'Netherlands',
-            'address' => 'Fukushima', 
+            'first_name'  => 'Naoya',
+            'last_name'   => 'Sato',
+            'user_name'   => 'iamnaoyasato',
+            'email'       => 'satonao@kredo.com',
+            'password'    => Hash::make('password914'),
+            'tel'         => '627288238',
+            'country'     => 'Netherlands',
+            'address'     => 'Fukushima', 
             'postal_code' => '9758WK',
         ]);
 
-        // 2. Xuan Ng
+        // 2. Xuan Wu
         User::create([
-            'first_name' => 'Xuan',
-            'last_name' => 'Ng',
-            'user_name' => 'ng.xuan',
-            'email' => 'ng.xuan@example.com',
-            'password' => Hash::make('password123'),
-            'tel' => '09012345678',
-            'country' => 'Vietnam',
-            'address' => 'Ho Chi Minh City',
-            'postal_code' => '700000',
+            'first_name'  => 'Xuan',
+            'last_name'   => 'Wu',
+            'user_name'   => 'iamwuxuan',
+            'email'       => 'wuxuan@kredo.com',
+            'password'    => Hash::make('password527'),
+            'tel'         => '60940527',
+            'country'     => 'China',
+            'address'     => 'shanghai',
+            'postal_code' => 'Tianjin',
         ]);
 
-        // 3. Haruto Tanaka
+        // 3. Haruto Fujiwara
         User::create([
-            'first_name' => 'Haruto',
-            'last_name' => 'Tanaka',
-            'user_name' => 'haru_tanaka',
-            'email' => 'haru@example.com',
-            'password' => Hash::make('password789'),
-            'tel' => '08098765432',
-            'country' => 'Japan',
-            'address' => 'Tokyo, Shibuya',
-            'postal_code' => '1500002',
+            'first_name'  => 'Haruto',
+            'last_name'   => 'Fujiwara',
+            'user_name'   => 'iamharuto',
+            'email'       => 'haruto@kredo.com',
+            'password'    => Hash::make('password0917'),
+            'tel'         => '6187846464',
+            'country'     => 'Japan',
+            'address'     => 'Akita',
+            'postal_code' => '0717WK',
         ]);
-    }
-}
-            'tel' => '627288238',
-            'email'=>'satonao@kredo.com',
-            'postal_code'=> '9758WK',
-            'country' => 'Netherlands',
-            'address' => 'Fukushima',
-            'password' => Hash::make('password914'),            
-
-        ]);
-
-        // second user
-        User::create([
-            'first_name' => 'Xuan',
-            'last_name' => 'Wu',
-            'user_name' => 'iamwuxuan',
-            'tel' => '60940527',
-            'email'=>'wuxuan@kredo.com',
-            'postal_code'=> 'Tianjin',
-            'country' => 'China',
-            'address' => 'shanghai',
-            'password' => Hash::make('password527'),            
-
-        ]);
-
-        // third
-        User::create([
-            'first_name' => 'Haruto',
-            'last_name' => 'Fujiwara',
-            'user_name' => 'iamharuto',
-            'tel' => '6187846464',
-            'email'=>'haruto@kredo.com',
-            'postal_code'=> '0717WK',
-            'country' => 'Japan',
-            'address' => 'Akita',
-            'password' => Hash::make('password0917'),            
-
-        ]);
-
-
     }
 }
