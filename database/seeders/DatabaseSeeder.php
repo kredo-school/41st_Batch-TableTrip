8569ae2dfd78c52ef5e2c7fd9519219e86543b2d
riku0117 <riku2001117@yahoo.co.jp>
2026-04-16 19:50:18 +0900

    no message

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,32 +12,27 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-
         \App\Models\Category::firstOrCreate(
             ['name' => 'Meal Kit']
         );
 
         $this->call([
-            UserSeeder::class,
-            PaymentMethodsSeeder::class,
+            UserSeeder::class,           
+            // RestaurantSeeder::class,     
+            PaymentMethodsSeeder::class, 
+            ProductSeeder::class,        
+            ReservationSeeder::class,    
+            InquirySeeder::class,        
+            // NotificationSeeder::class,   
         ]);
 
-        $this->call([
-            ProductSeeder::class,
-            ReservationSeeder::class,
-        ]);
-
-        
         User::factory()->create([
             'first_name' => 'Test',
             'last_name' => 'User',
-            'user_name' => 'Test User',
+            'user_name' => 'testuser',
             'email' => 'test@example.com',
-            'tel' => '0000000000',  
-            'country' => 'Japan',    
-        $this->call([
-            UserSeeder::class,           
-            PaymentMethodsSeeder::class, 
+            'tel' => '0000000000',
+            'country' => 'Japan',
         ]);
     }
 }
